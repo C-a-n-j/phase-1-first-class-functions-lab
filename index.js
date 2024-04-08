828d7eca2a47d95f9337e63606e30193a72167b6
C-a-n-j <eddymacharia450@gmail.com>
2024-04-08 12:02:38 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+//fuction sliced first two 
+
+
+function returnFirstTwoDrivers (){
+    let twoDrivers = drivers.slice(0, 2)
+    return twoDrivers;
+}
+//last two
+
+
+function returnLastTwoDrivers (){
+    let lastDrivers = drivers.slice(-2);
+    return lastDrivers;
+}
+// combining arryas
+
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(num){
+    return function(){
+        return num*num
+    }
+}
+//fare
+
+const fareDoubler = num2=>num2*2
+
+const fareTripler = num3=>num3*3
+
+function selectDifferentDrivers(drivers, func){
+   let selecteddrivers = [...drivers ,...func]
+
+   return func(selecteddrivers)
+}
+
